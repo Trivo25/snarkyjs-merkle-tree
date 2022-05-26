1e50df96f8c262116a66e350c08b8795810465b7
Florian <florian@technotro.com>
2022-05-26 15:51:41 +0200

    fix tests

--- src/MerkleTree.test.ts
+++ src/MerkleTree.test.ts
@@ -18,63 +18,78 @@ describe('MerkleTree', () => {
   describe('should construct merkle trees correctly', () => {
     describe('even', () => {
       it('n = 2', () => {
-        let tree = new MerkleTree([Field(0), Field(1)], options);
-        expect(
-          tree
-            .getMerkleRoot()!
-            .equals(Poseidon.hash([Field(0), Field(1)]))
-            .toBoolean()
-        );
-      });
-      it('n = 4', () => {
-        let tree = new MerkleTree(
-          [Field(0), Field(1), Field(2), Field(3)],
-          options
-        );
+        let rawData = [Field(0), Field(1)];
+        let tree = new MerkleTree(rawData, options);
         expect(
           tree
             .getMerkleRoot()!
             .equals(
               Poseidon.hash([
-                Poseidon.hash([Field(0), Field(1)]),
-                Poseidon.hash([Field(2), Field(3)]),
+                Poseidon.hash([rawData[0]]),
+                Poseidon.hash([rawData[1]]),
               ])
             )
             .toBoolean()
-        );
+        ).toEqual(true);
+      });
+      it('n = 4', () => {
+        let rawData = [Field(0), Field(1), Field(2), Field(3)];
+        let tree = new MerkleTree(rawData, options);
+        expect(
+          tree
+            .getMerkleRoot()!
+            .equals(
+              Poseidon.hash([
+                Poseidon.hash([
+                  Poseidon.hash([rawData[0]]),
+                  Poseidon.hash([rawData[1]]),
+                ]),
+                Poseidon.hash([
+                  Poseidon.hash([rawData[2]]),
+                  Poseidon.hash([rawData[3]]),
+                ]),
+              ])
+            )
+            .toBoolean()
+        ).toEqual(true);
       });
     });
 
     describe('odd', () => {
       it('n = 1', () => {
-        let tree = new MerkleTree([Field(0)], options);
+        let rawData = [Field(0)];
+        let tree = new MerkleTree(rawData, options);
         expect(
           tree
             .getMerkleRoot()!
-            .equals(Poseidon.hash([Field(0)]))
+            .equals(Poseidon.hash([rawData[0]]))
             .toBoolean()
-        );
+        ).toEqual(true);
       });
       it('n = 3', () => {
-        let tree = new MerkleTree([Field(0), Field(1), Field(2)], options);
+        let rawData = [Field(0), Field(1), Field(2)];
+        let tree = new MerkleTree(rawData, options);
         expect(
           tree
             .getMerkleRoot()!
             .equals(
               Poseidon.hash([
-                Poseidon.hash([Field(0), Field(1)]),
-                Poseidon.hash([Field(3)]),
+                Poseidon.hash([
+                  Poseidon.hash([rawData[0]]),
+                  Poseidon.hash([rawData[1]]),
+                ]),
+                Poseidon.hash([rawData[2]]),
               ])
             )
             .toBoolean()
-        );
+        ).toEqual(true);
       });
     });
   });
 
   describe('should produce and verify proofs correctly', () => {
     it('produce and verify proof', () => {
-      let rawData = [Field(0), Field(1), Field(3), Field(4)];
+      let rawData = [Field(0), Field(1), Field(2)];
       let tree = new MerkleTree(rawData, options);
       rawData.forEach((_, i) => {
         let proof = tree.getProof(i);
